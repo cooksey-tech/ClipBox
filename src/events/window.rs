7a1f8fa0a02b648ae03389d0d1e1325b59578832
unknown <andrewcooksey42@gmail.com>
2024-01-04 19:50:29 -0600

    added comments to explain what the parameters are

--- src/events/window.rs
+++ src/events/window.rs
@@ -49,34 +49,34 @@ pub fn create_window() {
         // register class
         let class_name = "ClipBox\0".as_ptr() as *const u16; // Ensure null-terminated string
         let mut wc = WNDCLASSEXW {
-            cbSize: std::mem::size_of::<WNDCLASSEXW>() as u32,
-            style: 0,
-            lpfnWndProc: Some(window_proc), // Provide a valid window procedure
-            cbClsExtra: 0,
-            cbWndExtra: 0,
-            hInstance: GetModuleHandleW(null_mut()) as HINSTANCE, // Use GetModuleHandleW
-            hIcon: HICON::default(),
-            hCursor: HCURSOR::default(),
-            hbrBackground: HBRUSH::default(),
-            lpszMenuName: null_mut(),
-            lpszClassName: class_name,
-            hIconSm:HICON::default(),
+            cbSize: std::mem::size_of::<WNDCLASSEXW>() as u32, // Size of structure (bytes)
+            style: 0, // Determines attributes like window appearance and behavior
+            lpfnWndProc: Some(window_proc), // Pointer to the window procedure function
+            cbClsExtra: 0, // Number of extra bytes to allocate for class data
+            cbWndExtra: 0, // Number of extra bytes to allocate for each window of this class
+            hInstance: GetModuleHandleW(null_mut()) as HINSTANCE, // Handle to the instance of the module that contains the window procedure
+            hIcon: HICON::default(), // Used to represent windows of this class in taskbars, title bars, etc
+            hCursor: HCURSOR::default(), // Used to represent windows of this class in taskbars, title bars, etc
+            hbrBackground: HBRUSH::default(), // Handle to the brush used to paint the background of windows of this class
+            lpszMenuName: null_mut(), // Pointer to a null-terminated string that specifies the name of the class menu, if any
+            lpszClassName: class_name, // Pointer to a null-terminated string that specifies the window class name
+            hIconSm:HICON::default(), // Used in places like the taskbar or title bar when icons are displayed in smaller sizes
         };
         let result = RegisterClassExW(&mut wc);
 
         let window = CreateWindowExW(
-            0,
-            wc.lpszClassName,
-            "ClipBox\0".as_ptr() as *const u16,
-            WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            HWND::default(),
-            HMENU::default(),
-            wc.hInstance, // Use the hInstance from WNDCLASSEX
-            null_mut(),
+            0, // Optional window styles
+            wc.lpszClassName, // Pointer to a null-terminated string specifying the name of the window class
+            "ClipBox\0".as_ptr() as *const u16, // Pointer to a null-terminated string that will be the window's title
+            WS_OVERLAPPEDWINDOW, // Window style, including title bar, sizing border, window menu, and minimize/maximize buttons
+            CW_USEDEFAULT, // Initial horizontal position
+            CW_USEDEFAULT, // Initial vertical position
+            CW_USEDEFAULT, // Initial width of the window
+            CW_USEDEFAULT, // Initial height of the window
+            HWND::default(), // Handle to the parent or owner window
+            HMENU::default(), // Handle to a menu, or specifies a child-window identifier depending on the window style
+            wc.hInstance, // Handle to the instance of the module that created the window
+            null_mut(), // Pointer to a value to be passed to the window through the WM_CREATE message
         );
 
         let error = GetLastError();
