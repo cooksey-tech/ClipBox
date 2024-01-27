e84072cdac534579905149b7526d71fc110ee1bc
unknown <andrewcooksey42@gmail.com>
2024-01-27 15:24:08 -0600

    started on get_file_icon function to access

---
+++ src/windows/icons.rs
@@ -0,0 +1,29 @@
+use std::{os::windows::ffi::OsStrExt, path::PathBuf};
+use windows_sys::Win32::{Foundation::MAX_PATH, UI::{Shell::{SHGFI_ICON, SHGFI_LARGEICON}, WindowsAndMessaging::HICON}};
+use windows_sys::Win32::UI::Shell::SHFILEINFOW;
+use windows_sys::Win32::UI::Shell::SHGetFileInfoW;
+
+use crate::tools::encoding::wide_char;
+
+fn get_file_icon(path: &PathBuf) -> HICON {
+    // Contains information about a file object.
+    let mut shfi = SHFILEINFOW {
+        hIcon: HICON::default(),
+        iIcon: 0,
+        dwAttributes: 0,
+        szDisplayName: [0; MAX_PATH as usize],
+        szTypeName: [0; 80],
+    };
+
+    unsafe {
+        SHGetFileInfoW(
+            wide_char(path.to_str().expect("Failed to convert path to string")),
+            0,
+            &mut shfi,
+            std::mem::size_of::<SHFILEINFOW>() as u32,
+            SHGFI_ICON | SHGFI_LARGEICON,
+        );
+    }
+
+    shfi.hIcon
+}
--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -1,2 +1,3 @@
 pub mod procedure;
 pub mod window;
+pub mod icons;
